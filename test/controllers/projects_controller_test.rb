565afca4994b01167f264cff519167972b0e2097
troccoli <pedroaugustobelzkai@gmail.com>
2026-04-26 09:10:46 -0300

    feat: projects CRUD controller and views
    - thin ProjectsController with admin guard on writes
    - slug-based lookup, fragment + http caching
    - replace home#index with projects#index as root
    - add Projects nav link in topbar

---
+++ test/controllers/projects_controller_test.rb
@@ -0,0 +1,124 @@
+require "test_helper"
+
+class ProjectsControllerTest < ActionDispatch::IntegrationTest
+  test "redirects to sign_in when unauthenticated" do
+    get projects_path
+    assert_redirected_to sign_in_path
+  end
+
+  test "translator can list and view projects" do
+    sign_in_as(users(:translator))
+
+    get projects_path
+    assert_response :success
+    assert_select "a", text: "New project", count: 0
+
+    get project_path(projects(:main_app))
+    assert_response :success
+  end
+
+  test "viewer can list and view projects" do
+    sign_in_as(users(:viewer))
+
+    get projects_path
+    assert_response :success
+
+    get project_path(projects(:main_app))
+    assert_response :success
+  end
+
+  test "non-admin cannot reach new/edit/create/update/destroy" do
+    sign_in_as(users(:translator))
+
+    get new_project_path
+    assert_redirected_to root_path
+    assert_match(/Admins only/, flash[:alert])
+
+    get edit_project_path(projects(:main_app))
+    assert_redirected_to root_path
+
+    assert_no_difference "Project.count" do
+      post projects_path, params: { project: { name: "Sneaky" } }
+    end
+    assert_redirected_to root_path
+
+    patch project_path(projects(:main_app)), params: { project: { name: "Hijacked" } }
+    assert_redirected_to root_path
+    assert_equal "Main App", projects(:main_app).reload.name
+
+    assert_no_difference "Project.count" do
+      delete project_path(projects(:main_app))
+    end
+    assert_redirected_to root_path
+  end
+
+  test "admin creates project with auto-generated slug" do
+    sign_in_as(users(:admin))
+
+    assert_difference "Project.count", 1 do
+      post projects_path, params: { project: { name: "Hello World" } }
+    end
+
+    project = Project.find_by!(slug: "hello-world")
+    assert_equal "Hello World", project.name
+    assert_redirected_to project_path(project)
+    assert_match(/Project created/, flash[:notice])
+  end
+
+  test "admin sees errors when creating with blank name" do
+    sign_in_as(users(:admin))
+
+    assert_no_difference "Project.count" do
+      post projects_path, params: { project: { name: "" } }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "admin updates project name" do
+    sign_in_as(users(:admin))
+    project = projects(:main_app)
+    original_slug = project.slug
+
+    patch project_path(project), params: { project: { name: "Renamed App" } }
+
+    assert_redirected_to project_path(project)
+    project.reload
+    assert_equal "Renamed App", project.name
+    assert_equal original_slug, project.slug
+  end
+
+  test "admin sees errors when updating with blank name" do
+    sign_in_as(users(:admin))
+
+    patch project_path(projects(:main_app)), params: { project: { name: "" } }
+    assert_response :unprocessable_entity
+  end
+
+  test "admin destroys project" do
+    sign_in_as(users(:admin))
+
+    assert_difference "Project.count", -1 do
+      delete project_path(projects(:main_app))
+    end
+    assert_redirected_to projects_path
+  end
+
+  test "404 on unknown slug" do
+    sign_in_as(users(:admin))
+    get project_path("does-not-exist")
+    assert_response :not_found
+  end
+
+  test "root routes to projects index" do
+    sign_in_as(users(:viewer))
+    get root_path
+    assert_response :success
+    assert_select "h1", "Projects"
+  end
+
+  private
+    def sign_in_as(user)
+      token = user.sign_in_tokens.create!
+      get sign_in_with_token_path(token: token.token)
+    end
+end
